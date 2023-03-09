9303dca8905580e681c4783ea1909615079c3b12
JavierEspinoza <javierespriv44@gmail.com>
2023-03-09 08:41:24 -0600

    Modificacion de rango de tiempo

--- Impresion/Impresion2cajas.py
+++ Impresion/Impresion2cajas.py
@@ -40,7 +40,8 @@ for i in range(columns):
 	sensorB_label.append(sheetB.cell(0,i).value)
 sensorA_label_prom = []
 sensorB_label_prom = []
-dates = []
+datesA = []
+datesB = []
 
 def Read_Data():												
 	for j in range(columns):										#Se busca recorrer las columnas por coordenada j
@@ -49,7 +50,10 @@ def Read_Data():
 				if j == 0:									#Se identifica la columna que contiene los datos de fecha
 					cell_date = sheetA.cell(i,j)						#Se obtiene el dato tipo str que se contiene en la celda con coordenadas (i,j)
 					objDate = datetime.strptime(cell_date.value, "%Y-%m-%d %H:%M:%S")	#Se convierte a tipo datetime year-month-day hour:min:second
-					dates.append(objDate)							#Se agrega el dato a la lista de fechas
+					datesA.append(objDate)
+					cell_date = sheetB.cell(i,j)						#Se obtiene el dato tipo str que se contiene en la celda con coordenadas (i,j)
+					objDate = datetime.strptime(cell_date.value, "%Y-%m-%d %H:%M:%S")	#Se convierte a tipo datetime year-month-day hour:min:second
+					datesB.append(objDate)							#Se agrega el dato a la lista de fechas
 				else:										#Se actúa en caso de que el dato no sea fecha
 														#Se obtiene el dato tipo int que se contiene en la celda con coordenadas (i,j)
 					sensoresA[j-1].append(int(sheetA.cell(i,j).value))				#Se agrega el dato a la lista [j-1] en el conjunto de listas de sensor 
@@ -58,21 +62,23 @@ def Read_Data():
 def Calc_Print():
 	promedioA = []												#Se declara una lista de valores promedio
 	promedioB = []
-	dates_val = matplotlib.dates.date2num(dates)								#Se convierte el dato tipo datetime a número interpretable por matplotlib
+	dates_valA = matplotlib.dates.date2num(datesA)								#Se convierte el dato tipo datetime a número interpretable por matplotlib
+	dates_valB = matplotlib.dates.date2num(datesB)
 	fig, ax = plt.subplots()										#Se define una figura nueva
 	for i in range(3):										#Se utiliza el número de columnas con el fin de conocer la cantidad de datos
 		promedioA.append(np.mean(sensoresA[i+2]))
 		promedioB.append(np.mean(sensoresB[i+2]))								#Se agrega el valor del promedio de los datos del sensor i a la lista de promedios								
 		sensorA_label_prom.append(sensorA_label[i+3]+' BC '+str(round(promedioA[i],2))) 			#Se añade el valor del promedio del conjunto a la etiqueta con 2 decimales
 		sensorB_label_prom.append(sensorB_label[i+3]+' - '+str(round(promedioB[i],2)))
-		ax.plot_date(dates_val, sensoresA[i+2],linestyle='solid',label=sensorA_label[i+3]+'BC')			#Se realiza un plot con x=fechas,y=valor del sensor, etiqueta sensor
-		ax.plot_date(dates_val, sensoresB[i+2],linestyle='solid',label=sensorB_label[i+3]+'-')
-	
+		ax.plot_date(dates_valA, sensoresA[i+2],linestyle='solid',label=sensorA_label[i+3]+'BC')			#Se realiza un plot con x=fechas,y=valor del sensor, etiqueta sensor
+		ax.plot_date(dates_valB, sensoresB[i+2],linestyle='solid',label=sensorB_label[i+3]+'-')
+	ax.set_ylabel("% Humedad")
+	ax.set_xlabel("Tiempo")
 	ax.grid()	
 	ax.legend()
 	fig, ax = plt.subplots()
 	ax.bar(sensorA_label_prom, promedioA,label='Promedio de Humedad',edgecolor='black',fill=1,color='red')
-	ax.set_yscale('log')
+	#ax.set_yscale('log')
 	ax.set_ylim(min(promedioA)-(min(promedioA)*0.2), max(promedioA)+(max(promedioA)*0.2))
 	
 	ax.grid()
@@ -80,7 +86,7 @@ def Calc_Print():
 	
 	fig, ax = plt.subplots()
 	ax.bar(sensorB_label_prom, promedioB,label='Promedio de Humedad',edgecolor='black',fill=1,color='green')
-	ax.set_yscale('log')
+	#ax.set_yscale('log')
 	ax.set_ylim(min(promedioB)-(min(promedioB)*0.2), max(promedioB)+(max(promedioB)*0.2))
 
 	ax.grid()
